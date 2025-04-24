8a47fd4283282b676acb1fdb369522c2c5362a80
Muhammad Bilal <shiekhbilalbs5@gmail.com>
2025-04-24 18:55:38 +0500

    feat: theme app

--- context-api/src/components/ChildC.jsx
+++ context-api/src/components/ChildC.jsx
@@ -1,11 +1,19 @@
 import React, { useContext } from "react";
-import { UserContext } from "../App";
+import { ThemeContext, UserContext } from "../App";
 
 const ChildC = () => {
   const user = useContext(UserContext);
+  const [theme, setTheme] = useContext(ThemeContext);
+  const handleToggle = () => {
+    if(theme === "light")
+      setTheme("dark")
+    else if (theme === "dark")
+      setTheme("light")
+  }
   return (
     <div>
       <p>{user.name}</p>
+      <button onClick={handleToggle}>Change Theme</button>
     </div>
   )
 };
